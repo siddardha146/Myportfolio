7bb49ce8fce89d9b3a5b28015429863647b53107
sajjala siddardha <siddardhabhaigaming@gmail.com>
2025-11-03 23:51:57 +0530

    Update useTypingEffect.tsx

--- src/hooks/useTypingEffect.tsx
+++ src/hooks/useTypingEffect.tsx
@@ -1,36 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useTypingEffect = (words: string[], typingSpeed = 100, deletingSpeed = 50, pauseTime = 2000) => {
-  const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [currentText, setCurrentText] = useState('');
-  const [isDeleting, setIsDeleting] = useState(false);
+export const useTypingEffect = (words, speed = 1000) => {
+  const [index, setIndex] = useState(0);
+  const [displayText, setDisplayText] = useState(words[0]);
 
   useEffect(() => {
-    const currentWord = words[currentWordIndex];
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % words.length);
+      setDisplayText(words[(index + 1) % words.length]);
+    }, speed);
 
-    const timeout = setTimeout(() => {
-      if (!isDeleting) {
-        // Typing
-        if (currentText.length < currentWord.length) {
-          setCurrentText(currentWord.slice(0, currentText.length + 1));
-        } else {
-          // Finished typing, pause then start deleting
-          setTimeout(() => setIsDeleting(true), pauseTime);
-        }
-      } else {
-        // Deleting
-        if (currentText.length > 0) {
-          setCurrentText(currentText.slice(0, -1));
-        } else {
-          // Finished deleting, move to next word
-          setIsDeleting(false);
-          setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-        }
-      }
-    }, isDeleting ? deletingSpeed : typingSpeed);
+    return () => clearInterval(interval);
+  }, [index]);
 
-    return () => clearTimeout(timeout);
-  }, [currentText, isDeleting, currentWordIndex, words, typingSpeed, deletingSpeed, pauseTime]);
-
-  return currentText;
+  return displayText;
 };
